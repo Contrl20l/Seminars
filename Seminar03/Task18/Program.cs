2061913b9e913101dc5955752b18e7228b94a8f7
dmitrykislidenko <prince.Rassia@yandex.ru>
2022-07-26 20:02:47 +0300

    Решение 18 задачи на семинаре

---
+++ Seminar03/Task18/Program.cs
@@ -0,0 +1,18 @@
+﻿//Печатает номер четверти
+void printQuater(int[,] arreyPoint)
+{
+
+    if (arreyPoint[0, 0] >  0 && arreyPoint[0, 1] > 0)
+        Console.WriteLine("1 четверть");
+
+    if (arreyPoint[0, 0] < 0 && arreyPoint[0, 1] > 0)
+        Console.WriteLine("2 четверть");
+
+    if (arreyPoint[0, 0] < 0 && arreyPoint[0, 1] < 0)
+        Console.WriteLine("3 четверть");
+
+    if (arreyPoint[0, 0] > 0 && arreyPoint[0, 1] < 0)
+        Console.WriteLine("4 четверть");
+}
+int[,] arreyPoint = readPoint();
+printQuater(arreyPoint);
